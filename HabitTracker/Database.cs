a2ea7dcce0e963dfb9fe8196863859392d52353c
bhark2200 <bhark2200@gmail.com>
2022-09-20 22:36:01 -0400

    Add project files.

---
+++ HabitTracker/Database.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.Sqlite;
+
+
+namespace HabitTracker
+{
+    static internal class Database
+    {
+        static string connectionString = @"Data Source=habit-Tracker.db";
+        static void CreateDatabase()
+        {
+            /*Creating a connetion passing the the connection string as an argument
+             * This will create the database for you, there's no need to manually create it.
+             * And no need to use File.Create().*/
+
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                //Creating the command to be sent to the database
+                using (var tableCmd = connection.CreateCommand())
+                {
+                    connection.Open();
+                    //Declaring what is that command (in SQL syntax)
+                    tableCmd.CommandText =
+                        @"CREATE TABLE IF NOT EXISTS codingHoursPerDay (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Date TEXT,
+                    Quantity INTEGER
+                    )";
+
+                    //Executing the command, which isn't a query, it's not asking to return data from the table.
+                    tableCmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ HabitTracker/Program.cs
@@ -0,0 +1,5 @@
+﻿using HabitTracker;
+
+
+Database.CreateDatabase();
+
